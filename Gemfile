0ba1d00efa62f6894f925ee319d2ed219eadef5c
yasmin crisostomo <crisostomyasmin@gmail.com>
2022-05-30 17:49:14 -0300

    add view show, view new and changed the method create

--- Gemfile
+++ Gemfile
@@ -38,6 +38,8 @@ gem 'devise'
 
 gem 'dotenv-rails', groups: [:development, :test]
 
+gem 'cloudinary', '~> 1.16.0'
+
 group :development, :test do
   gem 'pry-byebug'
   gem 'pry-rails'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,17 @@
 require 'faker'
 
+user = User.create(email: "teste@gmail.com", password: "123123")
+
 10.times do
   author = Faker::Book.author
   title = Faker::Book.title
   description = "Summit Lake, uma pequena cidade entre montanhas,
   é esse tipo de lugar, bucólico e com encantadoras casas
-  dispostas à beira de um longo trecho de água intocada.Duas semanas atrás, a estudante de direito Becca Eckersley foi
-  brutalmente assassinada em uma dessas casas"
+  dispostas à beira de um longo trecho de água intocada. Duas semanas atrás, a estudante de direito Becca Eckersley foi
+  brutalmente assassinada em uma dessas casas."
   # description = Faker::Movie.quote
   category = Faker::Book.genre
   price = rand(20..40)
 
-  Book.create!(user_id: 1, author: author, title: title, description: description, category: category, price: price)
+  Book.create!(user: user, author: author, title: title, description: description, category: category, price: price)
 end

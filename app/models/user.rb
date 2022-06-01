caa6aa9d58ff0479bd1a54f32c8fa8f6c02c9248
yasmin crisostomo <crisostomyasmin@gmail.com>
2022-06-01 14:48:45 -0300

    nav bar

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
 
   has_many :books
   has_many :orders, dependent: :destroy
+  has_one_attached :avatar
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,

a5f1411b42de04dd794d594d2cc2b60abfbe670f
yasmin crisostomo <crisostomyasmin@gmail.com>
2022-06-03 15:53:16 -0300

    validation

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,7 +3,7 @@ class Order < ApplicationRecord
   belongs_to :book
 
   validates :name, :card_number, :card_code, :card_expiration, presence: true
-  validates :card_number, length: { is: 16 }
+  validates :card_number, length: { is: 19 }
   validates :card_expiration, length: { is: 5 }
   validates :card_code, length: { is: 3 }
 end
